a003ac413a5d68c41bbcc270da5575b56a68ad38
LAPTOP-QOOVCJT1\Dudley Dursley <ramiro.gonzalez@sandbox.game>
2023-05-01 18:23:27 -0300

    dapp updated to send messages and update name in both networks

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,25 @@ import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import './App.css';
 import WavePortalJson from './utils/WavePortal.json';
-import { CONTRACT_ADDRESS, GOERLI_CHAIN_ID, GOERLI_NAME } from './constants';
+import CONSTANTS from "./constants";
 const contractABI = WavePortalJson.abi;
 
 export default function App() {
-  
+
   const [currentAccount, setCurrentAccount] = useState(null);
-  const [totalWaves, setTotalWaves] = useState(0);
+  const [contractAddress, setContractAddress] = useState(null);
+  
   const [mineLoading, setMineLoading] = useState(false);
+  const [totalWaves, setTotalWaves] = useState(0);
+  const [allWaves, setAllWaves] = useState([]);
+  
+  
+  const [name, setName] = useState("");
+  const [waveMessage, setWaveMessage] = useState("");
+  const [nameInput, setNameInput] = useState(false);
+  
   const [wrongNetworkError, setWrongNetworkError] = useState(false);
+
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -29,7 +39,11 @@ export default function App() {
         const account = accounts[0];
         console.log("Encontrada a conta autorizada:", account);
         setCurrentAccount(account);
-        await getChainId();
+        const network = await getChainInfo();
+        const actualContractAddress = await getContractAddress(network);
+        await getAllWaves(actualContractAddress);
+        await getTotalWaves(account, actualContractAddress);
+        await getName(actualContractAddress);
       } else {
         console.log("Nenhuma conta autorizada foi encontrada")
       }
@@ -41,7 +55,6 @@ export default function App() {
   const connectWallet = async () => {
     try {
       const { ethereum } = window;
-debugger
       if (!ethereum) return;
 
       const accounts = await ethereum.request({ method: "eth_requestAccounts" });
@@ -64,6 +77,11 @@ debugger
         alert("Please connect your wallet again");
         return;
       }
+
+      if (!waveMessage) {
+        alert("Give me a cooler wave sending me a message");
+        return;
+      }
       
       const { ethereum } = window;
 
@@ -71,50 +89,72 @@ debugger
 
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const wavePortalContract = new ethers.Contract(CONTRACT_ADDRESS, contractABI, signer);
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
 
-        let count = await wavePortalContract.getTotalWaves();
-        console.log("Recuperado o número de tchauzinhos...", count.toNumber());
+        let count = await wavePortalContract.totalWaves();
+        console.log("Retreiving total waves count...", count.toNumber());
 
         setMineLoading(true);
         
-        const waveTxn = await wavePortalContract.wave();
+        const waveTxn = await wavePortalContract.wave(waveMessage);
 
-        console.log("Minerando...", waveTxn.hash);
-
+        console.log("Mining wave tx...", waveTxn.hash);
         await waveTxn.wait();
-        console.log("Minerado -- ", waveTxn.hash);
+        console.log("Mined tx ->", waveTxn.hash);
         
         setMineLoading(false);
         
-        count = await wavePortalContract.getTotalWaves();
-        console.log("Total de tchauzinhos recuperado...", count.toNumber());
+        count = await wavePortalContract.totalWaves();
+        console.log("Total wave count...", count.toNumber());
 
         setTotalWaves(count.toNumber());
+
+        await getAllWaves();
+
       } else {
-        console.log("Objeto Ethereum não encontrado!");
+        console.log("Ethereum object not found");
+      }
+    } catch (error) {
+      console.log(error);
+      setMineLoading(false);
+    }
+  }
+
+  const getName = async (actualContractAddress) => {
+    const contract = !contractAddress ? actualContractAddress : contractAddress;
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contract, contractABI, signer);
+        const name = await wavePortalContract.getName();
+        console.log(`Retrieving waver's name: ${name}`);
+        setName(name);
+      } else {
+        console.log("Ethereum object not found");
       }
     } catch (error) {
       console.log(error)
     }
-  }
+  };
 
-  const getTotalWaves = async (account) => {
+  const getTotalWaves = async (account, actualContractAddress) => {
+    console.log(account, actualContractAddress);
     if (!account && !currentAccount) return;
+    const contract = !contractAddress ? actualContractAddress : contractAddress;
     try {
       const { ethereum } = window;
 
       if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const wavePortalContract = new ethers.Contract(CONTRACT_ADDRESS, contractABI, signer);
-
-        let count = await wavePortalContract.getTotalWaves();
-        console.log("Recuperado o número de tchauzinhos...", count.toNumber());
-
+        const wavePortalContract = new ethers.Contract(contract, contractABI, signer);
+        let count = await wavePortalContract.totalWaves();
+        console.log("Retreiving total waves count...", count.toNumber());
         setTotalWaves(count.toNumber());
       } else {
-        console.log("Objeto Ethereum não encontrado!");
+        console.log("Ethereum object not found");
       }
     } catch (error) {
       console.log(error)
@@ -126,46 +166,89 @@ debugger
 
     if (!ethereum) return;
 
-    ethereum.on("chainChanged", () => {
-      alert("chain changed");
+    ethereum.on("chainChanged", async () => {
+      // alert("chain changed");
       setWrongNetworkError(true);
-      getChainId();
+      const network = await getChainInfo();
+      const actualContractAddress = await getContractAddress(network);
+      await getAllWaves(actualContractAddress);
+      await getTotalWaves(null, actualContractAddress);
+      await getName(actualContractAddress);
     });
     
     ethereum.on("accountsChanged", async () => {
-        // alert("account changed");
-        setCurrentAccount(null);
-        setTotalWaves(0);
-        await connectWallet();
-    });
-    
-    ethereum.on("disconnect", () => {
-        alert("disconnect");
+      // alert("account changed");
+      setCurrentAccount(null);
+      setTotalWaves(0);
+      await connectWallet();
     });
   };
 
-  const getChainId = async () => {
+  const getChainInfo = async () => {
     console.log("Getting nework name and id...");
     try {
       const chainId = Number(await window.ethereum.request({ method: "eth_chainId" }));
       const network = ethers.providers.getNetwork(chainId);
       console.log(`Network name: ${network.name}, id:${network.chainId}`);
-      setWrongNetworkError(!isGoerliNetwork(network));
+      setWrongNetworkError(!isSupportedNetwork(network));
+      return network;
     } catch (error) {
       console.log(error);
     }
   };
 
+  const getContractAddress = async (network) => {
+    const actualContractAddress = getContractAddressFromNetwork(network); 
+    console.log(`Contract address set with address: ${actualContractAddress}`);
+    setContractAddress(actualContractAddress);
+    return actualContractAddress;
+  };
+
   const isGoerliNetwork = ({ name, chainId }) => {
-    return chainId === GOERLI_CHAIN_ID && name === GOERLI_NAME;
+    const goerli = CONSTANTS.ALLOWED_NETWORKS.goerli;
+    return chainId === goerli.chainId && name === goerli.name;
   };
   
-  useEffect(() => {
-    checkIfWalletIsConnected();
-    getTotalWaves(currentAccount);
-    setListeners();
-  }, []);
+  const isSepoliaNetwork = ({ name, chainId }) => {
+    const sepolia = CONSTANTS.ALLOWED_NETWORKS.sepolia;
+    return chainId === sepolia.chainId && name === sepolia.name;
+  };
   
+  const getContractAddressFromNetwork = (network) => {
+    if (isGoerliNetwork(network)) return CONSTANTS.CONTRACT_ADDRESSES.goerli;
+    if (isSepoliaNetwork(network)) return CONSTANTS.CONTRACT_ADDRESSES.sepolia;
+  };
+
+  const isSupportedNetwork = (network) => {
+    return isGoerliNetwork(network) || isSepoliaNetwork(network);
+  };
+  
+  const getAllWaves = async (actualContractAddress) => {
+    const contract = !contractAddress ? actualContractAddress : contractAddress;
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contract, contractABI, signer);
+        const waves = await wavePortalContract.getAllWaves();
+
+        const wavesCleaned = waves.map(wave => ({
+          address: wave.waver,
+          name: wave.name,
+          timestamp: new Date(wave.timestamp * 1000),
+          message: wave.message
+        })).reverse();
+        
+        setAllWaves(wavesCleaned);
+      } else {
+        console.log("Ethereum object not found!")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   const disconnectWallet = async () => {
     refreshState();
   };
@@ -176,6 +259,40 @@ debugger
     setMineLoading(false);
     setWrongNetworkError(false);
   };
+
+  const setNameOnBlockchain = async () => {
+    if(!name) {
+      alert("Name cannot be empty");
+      return;
+    }
+    try {
+      const { ethereum } = window;
+      if (!ethereum) { 
+        console.log("Ethereum object not found!")
+        return;
+      }
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+      setMineLoading(true);
+      const tx = await wavePortalContract.setName(name);
+      console.log("Mining name tx...", tx.hash);
+      await tx.wait();
+      console.log("Mined tx ->", tx.hash);
+      setMineLoading(false);
+      console.log("Name set as...", name);
+      setName(name);
+      setNameInput(false)
+    } catch (error) {
+      console.log(error);
+      setMineLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+    setListeners();
+  }, []);
 
   return (
     <div className="mainContainer">
@@ -192,9 +309,30 @@ debugger
           currentAccount
           ? (
               <div className="headerContainer">
-                <p>👋 Hi {currentAccount}</p>
-                { !wrongNetworkError && <button className="waveButton btnHeader" onClick={disconnectWallet}>Logout</button> }
+                <div>{ !name 
+                    ? <p>👋 Hi {currentAccount}</p>
+                    : <div>
+                      <p>👋 Hi {name}</p>
+                      <p className="bio">Address: {currentAccount}</p> 
+                    </div> 
+                  }
+                </div>
+                { !wrongNetworkError && 
+                  <div>
+                    <button className="waveButton btnHeader" onClick={disconnectWallet}>Logout</button>
+                    { !nameInput 
+                      ? <button className="waveButton btnHeader" onClick={() => setNameInput(true)}>Update name</button>
+                      : 
+                        <div>
+                          <input type="text" value={name} onChange={e=>setName(e.target.value)}/>
+                          <button className="waveButton btnHeader cancel" onClick={() => setNameInput(false)}>Cancel</button>
+                          <button className="waveButton btnHeader" onClick={() => setNameOnBlockchain()}>Submit</button>
+                        </div>
+                    }
+                  </div>
+                }
               </div>
+
             )
           : (
               <div className="headerContainer">
@@ -228,21 +366,39 @@ debugger
           )
         }
 
-        <div className="btnContainer">
+        <div className="">
         {
           !mineLoading ?
           (
-            <button
-              disabled={!currentAccount || wrongNetworkError}
-              className="waveButton"
-              onClick={wave}
-            >
-              Send wave 🌟
-            </button>
+            <div className="input">
+              <div>
+                <p>Leave me a message:</p>
+                <textarea placeholder="Give me a cool wave ..." onChange={e => setWaveMessage(e.target.value)}/>
+              </div>
+              <div className="btnContainer">
+                <button
+                  disabled={!currentAccount || wrongNetworkError}
+                  className="waveButton"
+                  onClick={wave}
+                  >
+                  Send wave 🌟
+                </button>
+              </div>
+            </div>
           )
-          : (<div className="loading"></div>)
+          : (<div className="btnContainer"><div className="loading"></div></div>)
         }
         </div>
+
+        {allWaves.map((wave, index) => {
+          return (
+            <div key={index} style={{ backgroundColor: "OldLace", marginTop: "16px", padding: "8px" }}>
+              <div>Address: {wave.address}</div>
+              { !!wave.name && (<div>Name: {wave.name}</div>) }
+              <div>Datetime: {wave.timestamp.toString()}</div>
+              <div>Message: {wave.message}</div>
+            </div>)
+        })}
       </div>
     </div>
   );
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,17 @@
-module.exports = {
-  CONTRACT_ADDRESS: "0xB10e6469C48B3097E4EAB714E3cFD8b5558dd9b0",
-  GOERLI_CHAIN_ID: 5,
-  GOERLI_NAME: "goerli",
+const CONTRACT_ADDRESSES = {
+  goerli: "0x5b1A0ECaE2e31E829D33E1A6053D2661cDd2AC8d",
+  sepolia: "0xb683b3BD996e06f67fA8e797d74a0cCB15925175",
 };
+
+const ALLOWED_NETWORKS = {
+  goerli: {
+    chainId: 5,
+    name: "goerli",
+  },
+  sepolia: {
+    chainId: 11155111,
+    name: "sepolia",
+  },
+};
+
+export default { CONTRACT_ADDRESSES, ALLOWED_NETWORKS };
